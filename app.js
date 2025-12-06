f5264cc44801292e83ba9ee24599347b882a0370
pacopprz19-dot <pacopprz19@gmail.com>
2025-12-06 18:24:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+
 
 // 🔑 Cliente OpenAI
 const openai = new OpenAI({
